f03e7369a4af353b91160408385296a6050e7e88
Antexo <antenehcodding@gmail.com>
2023-05-26 19:04:14 -0400

    modified:   frontend/src/components/LandingPage.js

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,116 @@
+//give a good code for landing page here like remitly.com or mpesa.com
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/authActions';
+import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Table } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Alert } from 'reactstrap';
+import { Badge } from 'reactstrap';
+import { Media } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { Progress } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Tooltip } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { UncontrolledAlert } from 'reactstrap';
+import { UncontrolledButtonDropdown, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { ButtonDropdown, ButtonGroup } from 'reactstrap';
+import { Fade } from 'reactstrap';
+import { CardLink } from 'reactstrap';
+import { CardDeck } from 'reactstrap';
+import { CardColumns } from 'reactstrap';
+import { CardGroup } from 'reactstrap';
+import { UncontrolledCarousel } from 'reactstrap';
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+import { UncontrolledCollapse, Button } from 'reactstrap';
+import { UncontrolledAlert } from 'reactstrap';
+import { UncontrolledTooltip } from 'reactstrap';
+import { Spinner } from 'reactstrap';
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { FormFeedback } from 'reactstrap';
+import { FormText } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+
+class LandingPage extends Component {
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+    render() {
+        const { user } = this.props.auth;
+        return (
+        <div>
+            <Container>
+            <Row>
+                <Col xs="6" sm="4">
+                <Card>
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                </Col>
+                <Col xs="6" sm="4">
+                <Card>
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                </Col>
+                <Col sm="4">
+                <Card>
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                </Col>
+            </Row>
+            </Container>
+        </div>
+        );
+    }
+    }
+    LandingPage.propTypes = {   
+    logoutUser: PropTypes.func.isRequired,  
+    auth: PropTypes.object.isRequired
+    };
+    const mapStateToProps = state => ({
+    auth: state.auth
+    });
+    export default connect(
+    mapStateToProps,
+    { logoutUser }
+    )(LandingPage);
+    
